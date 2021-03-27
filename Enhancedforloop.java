4ceac7424b470ba5f8e15c77ba89a492017ae707
sadiah7 <sadiah0712@gmail.com>
2021-03-27 17:12:14 +0530

    update in enhanced for loop

--- Enhancedforloop.java
+++ Enhancedforloop.java
@@ -2,7 +2,7 @@ class Enhancedforloop
 {
   public static void main(String[] args)
   {
-    int[] a = {10,20,30,40,50};
+    int[] a = {10,20,30,40,50,60};
     for(int i : a)  // traverses for every value in the loop and prints the value of each array element
     {
       System.out.println(i);
