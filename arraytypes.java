7b06ff13a27c754e2fb8c51b4aa14efc2653d094
sadiah7 <sadiah0712@gmail.com>
2021-03-27 18:26:42 +0530

    Update arraytypes.java

--- arraytypes.java
+++ arraytypes.java
@@ -3,11 +3,11 @@ class arraytypes
   public static void main(String[] args)
   {
     int arr[] = new int[7];
-    arr[0]=10;
-    arr[1]=20;
-    arr[2]=30;
-    arr[3]=40;
-    arr[4]=50;
+    arr[0]=100; // multiplied every element by 10
+    arr[1]=200;
+    arr[2]=300;
+    arr[3]=400;
+    arr[4]=500;
     for(int i=0;i<arr.length;i++)
     {
       System.out.println(arr[i]);
